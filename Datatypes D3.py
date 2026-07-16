0e9bd2de64719670a31b2bbba145ad32a558fb2f
Raavi7965 <venkiravi796@gmail.com>
2026-07-17 00:29:21 +0530

    update my Day3 codes

--- Datatypes D3.py
+++ Datatypes D3.py
@@ -29,3 +29,47 @@ com3 = 5.4+4j
 print(type(Com1))
 
 #String Data Types
+#It is a group of characters given between the quotes.
+
+name="venkatesh" 
+username='venkatesh7965'
+Password="venkatesh@123"
+empty = " "
+print(type(name))
+print(type(username))
+print(type(Password))
+print(type(empty))
+
+#Boolean Data Types
+#It is a data type which can have only two values True or False. 
+# It is used to represent the truth value of an expression.
+print(5==3)
+print(5>3)
+
+
+#sequence Data Types
+#Object 
+#List and Tuple are the data types which can store multiple values in a single variable.
+#List: It is a collection of values which can be of different data types. It is
+
+#List
+Sem1Marks=[90,80,70,60,50]
+print(type(Sem1Marks))
+print(Sem1Marks)
+
+Profile = ['Venkatesh', 'B.Tech',25,None,820,['Python','Java','C']]
+print(type(Profile))
+print(Profile)
+
+#Tuple: It is a collection of values which can be of different data types. 
+# It is similar to list but it is immutable i.e. we cannot change the values once it is created.
+
+tuple1=(1,2,3,4,5)
+print(type(tuple1))
+
+
+
+
+
+
+
