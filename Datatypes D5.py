dbe051731cea427660408baee284bfd4b3465c0b
Raavi7965 <venkiravi796@gmail.com>
2026-07-19 10:24:55 +0530

    update my Day5 codes

---
+++ Datatypes D5.py
@@ -0,0 +1,14 @@
+List1 = ['python', 'java', 'c++', 'c#', 'javascript']
+#values
+print(List1)
+# type of List1
+print(type(List1))
+#id
+print(id(List1))
+
+#cheaking print is an object or not
+print(print)
+#type
+print(type(print))
+#id
+print(id(print))
