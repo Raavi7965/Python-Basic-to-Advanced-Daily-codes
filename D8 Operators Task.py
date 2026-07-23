3228a38223550af3f5197ce701d2d58e77a9bef2
Raavi7965 <venkiravi796@gmail.com>
2026-07-23 10:12:04 +0530

    update my Day 8 codes

---
+++ D8 Operators Task.py
@@ -0,0 +1,32 @@
+# Checking whether a person is under 18 years old
+
+age = int(input('Enter your age: '))
+print(age < 18)
+
+
+# Checking whether the temperature is less than 30 degrees
+
+temperature = int(input('Enter the temperature: '))
+print(temperature < 30)
+
+
+# Checking whether the shopping amount is less than or equal to ₹500
+
+amount = int(input('Enter your shopping amount: '))
+print(amount <= 500)
+
+
+# Checking whether the number of tickets is less than or equal to 5
+
+tickets = int(input('Enter the number of tickets: '))
+print(tickets <= 5)
+
+# Checking whether a student has passed the examination
+
+marks = int(input('Enter your marks: '))
+print(marks >= 40)
+
+# Checking whether the bank balance is greater than or equal to ₹1000
+
+balance = int(input('Enter your bank balance: '))
+print(balance >= 1000)
---
+++ Operators D8.py
@@ -0,0 +1,108 @@
+# Assignment Operators
+# =
+
+name= "Venkatesh"
+print(name)
+
+
+# augmented assignment operator
+# +=, -=,*=,/=,//=,**=
+
+a=10
+a=a+6
+print(a)
+
+# Tracking number of steps walked in a day 
+steps=0
+steps+=300
+steps+=200
+steps+=500
+print(steps)
+
+# Argument assignment 
+# Deducting bank balance after every transaction
+
+bankBalance=50000
+bankBalance-=10000
+bankBalance-=5000
+bankBalance-=2000
+print(bankBalance)
+
+# Argument assignment for multiplication
+val1=10
+val1*=5
+print(val1)
+
+#Doubling the salary of an employee
+Salary=50000
+Salary*=2
+print(Salary)
+
+# Splitting a Bill among 4 members
+
+Bill =5740
+Bill/=4
+print(Bill)
+
+# Distributing 478 pens to 12 numbers
+pens=478
+pens//=12
+print(pens)
+
+
+# Checking wheather a number is even or odd 
+num = 10
+num%=2
+print(num)
+
+# Getting last Digit in a number 
+
+num1 = 123
+num1 %=10
+print(num1)
+
+# Calculate area of a square 
+
+Side=10
+Side**=2
+print(Side)
+
+# COMPARISION OPERATORS
+# Equal to ==
+
+employee=input('Enter your role: ')
+print(employee=='admin') 
+
+# Checking given password is equal to saved password
+savedpass='Raavi@7965'
+password=input('Enter your Password: ')
+print(password==savedpass)
+
+# not equal to !=
+sate=input('Enter a state which is not a capital: ')
+print(sate!='TG')
+
+# > it Check whether the first object is greater than the second object or not
+# Checking a person age is a grater than or not
+age=int(input('Enter your age: '))
+print(age>21)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
